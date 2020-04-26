72112a4be5ac414118bb85815ece7a7777f85e3e
Andy Lebowitz <alebo@sbcglobal.net>
2020-04-26 11:54:43 -0400

    changed logo

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import "../App.css"
+import dAlgonisticLogo from "../dAlgonisticLogo.png"
 
 export default class Navbar extends Component {
 
   render() {
     return (
       <nav style={{display: "flex", flexDirection: "row", alignItems: "center", height: 80}}>
-        <img style={{marginRight: ".75em"}} width={'150px'} height={'150px'} src="https://www.freelogodesign.org/file/app/client/thumb/d6105bd8-530c-473d-bff5-462413216ddf_200x200.png?1587883805222" />
+        <img style={{marginRight: ".75em"}} width={90} height={90} src={dAlgonisticLogo} />
         <Link to="/" className="navBarLink" style={{marginLeft: 40}}>Tasks</Link>
         <Link to="/create" className="navBarLink" style={{marginLeft: 40}}>Upload File</Link>
         <Link to="/user" className="navBarLink" style={{marginLeft: 40}}>{localStorage.getItem("loggedInUsername") === "" ? "Sign Up" : "Log Out"}</Link>
