8bc2086764a3f62d92ae7e46ad8dc47ac4dfe65b
Andy Lebowitz <alebo@sbcglobal.net>
2020-04-26 11:40:33 -0400

    removed name website title

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -8,7 +8,6 @@ export default class Navbar extends Component {
     return (
       <nav style={{display: "flex", flexDirection: "row", alignItems: "center", height: 80}}>
         <img style={{marginRight: ".75em"}} width={'150px'} height={'150px'} src="https://www.freelogodesign.org/file/app/client/thumb/d6105bd8-530c-473d-bff5-462413216ddf_200x200.png?1587883805222" />
-        <Link to="/" style={{fontSize: 28, color: "rgb(255, 77, 77)", fontFamily: "sans-serif", fontWeight: 700}}>Name</Link>
         <Link to="/" className="navBarLink" style={{marginLeft: 40}}>Tasks</Link>
         <Link to="/create" className="navBarLink" style={{marginLeft: 40}}>Upload File</Link>
         <Link to="/user" className="navBarLink" style={{marginLeft: 40}}>{localStorage.getItem("loggedInUsername") === "" ? "Sign Up" : "Log Out"}</Link>
